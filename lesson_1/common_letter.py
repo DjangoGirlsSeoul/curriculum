fb84fbdae9c73c6039ba57bf8d0f2967d732d24f
Jin Park <jin+github@jinpark.net>
2015-10-19 17:47:45 +0900

    first commit

---
+++ lesson_1/common_letter.py
@@ -0,0 +1,17 @@
+# Write a function that takes in a string. Your function should return the
+# most common letter in the string, and a count of how many times it
+# appears.
+#
+# Difficulty: medium.
+
+def most_common_letter(string):
+    return "hi"
+
+
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+print('most_common_letter("abca") == ["a", 2]: ' + str(most_common_letter('abca') == ['a', 2]))
+
+print('most_common_letter("abbab") == ["b", 3]: ' + str(most_common_letter('abbab') == ['b', 3]))
---
+++ lesson_1/reverse.py
@@ -0,0 +1,18 @@
+# Write a function that takes in a string and returns a new
+# string with the letters in reverse order. Assume that the input will always be strings.
+#
+# Difficulty: easy.
+
+def reverse(string):
+    # change the code below
+    return "hi"
+
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true. Do not change these
+
+print('reverse("abc") == "cba": ' + str(reverse("abc") == "cba"))
+
+print('reverse("a") == "a": ' + str(reverse("a") == "a"))
+
+print('reverse("") == "": ' + str(reverse("") == ""))
