82e43afc89c687a4ed2678e005cfac290cac83d4
DayoungPark <dda502@naver.com>
2015-11-06 11:16:54 +0900

    Update gross_pay.py

--- lesson_2/exercises/warm-up/gross_pay.py
+++ lesson_2/exercises/warm-up/gross_pay.py
@@ -1,8 +1,12 @@
-#2.3 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay.
+#2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+#2.3 input을 사용하여 사용자의 시급과 근무시간을 바탕으로 급여를 계산하는 프로그램을 작성하여 보세요.
 #Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25).
-#You should use raw_input to read a string and float() to convert the string to a number.
+#테스트를 위하여 35 시간과 2.75의 시급을 사용하세요. (급여는 96.25가 되어야 합니다.)
+#You should use input to read a string and float() to convert the string to a number.
+#string을 읽어내기 위해서는 input을, string을 숫자로 바꾸기 위해서는 float()를 사용해야 합니다.
 #Do not worry about error checking or bad user data.
+#에러 확인이나 유저 데이터는 신경쓰지 않아도 됩니다.
 
 # This first line is provided for you
 
-hrs = raw_input("Enter Hours:")
+hrs = input("Enter Hours:")
